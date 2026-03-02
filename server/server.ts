d00a0568f31db750d1e0a4f15be5dd8e570226b6
Rohit Rathore <rohitjeevanrathore@gmail.com>
2026-03-02 16:44:26 +0530

    build api for ai website creation

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import userRouter from "./routes/userRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
 
@@ -19,9 +21,14 @@ const port = process.env.PORT || 3000;
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
+app.use(express.json({limit: '50mb'}))
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/project', projectRouter)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
