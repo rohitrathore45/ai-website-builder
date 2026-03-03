018d7912cbc1e109d97bcf7cc9417c9eea40a52b
Rohit Rathore <rohitjeevanrathore@gmail.com>
2026-03-03 14:58:45 +0530

    created index.ts for vercel

--- server/server.ts
+++ server/server.ts
@@ -31,3 +31,11 @@ app.use('/api/project', projectRouter);
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+}
+
+export default app;
