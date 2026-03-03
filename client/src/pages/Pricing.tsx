b9125f40cbf5b4c348e26dca8db32003725fc89c
Rohit Rathore <rohitjeevanrathore@gmail.com>
2026-03-03 14:36:16 +0530

    added stripe payment gateway

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { appPlans } from '../assets/assets';
 import Footer from '../components/Footer';
+import { authClient } from '@/lib/auth-client';
+import { toast } from 'sonner';
+import api from '@/configs/axios';
 
 interface Plan {
   id: string;
@@ -13,12 +16,23 @@ interface Plan {
 
 const Pricing = () => {
 
+    const {data: session} = authClient.useSession();
+
   const [plans] = React.useState<Plan[]>(appPlans)
 
   const handlePurchase = async (planId : string) => {
-
+    try {
+        if(!session?.user) {
+            return toast('Please login to purchase credits')
+        }
+        const {data} = await api.post('/api/user/purchase-credits', {planId})
+        window.location.href = data.payment_link;
+    } catch (error: any) {
+        toast.error(error?.response?.data?.message || error.message);
+        console.log(error);
+    }
   }
-
+ 
   return (
     <>
       <div className='w-full max-w-5xl mx-auto z-20 max-md:px-4 min-h-[80vh]'>
--- server/server.ts
+++ server/server.ts
@@ -5,18 +5,18 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
 import userRouter from "./routes/userRoutes.js";
 import projectRouter from "./routes/projectRoutes.js";
+import { stripeWebhook } from "./controllers/stripeWebhook.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const corsOptions = {
     origin: process.env.TRUSTED_ORIGINS?.split(",") || [],
     credentials: true
 }
 
-
-
 app.use(cors(corsOptions));
+app.post('/api/stripe',  express.raw({type: 'application/json'}), stripeWebhook)
 app.use(express.json({ limit: '50mb' }));
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
